b474e032f16f788ea72dad60253ad7cf81c814d1
ela-iwaszkiewicz <iwaszkiewicz@evolbio.mpg.de>
2026-03-11 15:05:33 +0100

    texts2

--- src/components/LandingpageText.tsx
+++ src/components/LandingpageText.tsx
@@ -7,6 +7,7 @@ import Link from '@docusaurus/Link';
 const LandingpageText: React.FC = () => {
   return (
     <div className={styles.Content}>
+      <div className={styles.HeaderContainer}>
         <h4 className={styles.Description}>
           The DarkTree project aims to sequence 3,000 insect genomes from the most poorly studied radiations in the insect tree of life. Many of these “dark taxa” have been neglected by taxonomists and ecologists for centuries because they are so diverse and challenging to identify. Nevertheless, they occupy key niches in insect communities, often as decomposers or parasitoids, and represent millions of years of insect genome evolution that has not been studied to date.
         </h4>
@@ -17,6 +18,7 @@ const LandingpageText: React.FC = () => {
   As part of the project, we are developing and refining a state-of-the-art pipeline using genome sequencing, computer vision, and probabilistic machine learning. Read more about the project{' '}
   <Link to="/docs/first_glance">here</Link>, and how you can contribute to and benefit from this effort to advance the frontier in insect diversity research.
         </h4>
+      </div>
     </div>
   );
 };
