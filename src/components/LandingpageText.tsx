a7818107b0e8b0f3bc76ebd55276b4206eb3d7f2
ela-iwaszkiewicz <iwaszkiewicz@evolbio.mpg.de>
2026-03-11 14:50:40 +0100

    not_bold

--- src/components/LandingpageText.tsx
+++ src/components/LandingpageText.tsx
@@ -8,13 +8,13 @@ const LandingpageText: React.FC = () => {
   return (
     <div className={styles.Content}>
       <div className={styles.HeaderContainer}>
-        <h4 className={styles.DescriptionBold}>
+        <h4 className={styles.Description}>
           The DarkTree project aims to sequence 3,000 insect genomes from the most poorly studied radiations in the insect tree of life. Many of these “dark taxa” have been neglected by taxonomists and ecologists for centuries because they are so diverse and challenging to identify. Nevertheless, they occupy key niches in insect communities, often as decomposers or parasitoids, and represent millions of years of insect genome evolution that has not been studied to date.
         </h4>
-        <h4 className={styles.DescriptionBold}>
+        <h4 className={styles.Description}>
           DarkTree is based on material from tens of thousands of Malaise trap samples from around the world, which have gone through DNA metabarcoding. This allows us to reliably identify the darkest regions in the insect tree, and select a balanced set of representative species for genome sequencing.
           </h4>
-          <h4 className={styles.DescriptionBold}>
+          <h4 className={styles.Description}>
   As part of the project, we are developing and refining a state-of-the-art pipeline using genome sequencing, computer vision, and probabilistic machine learning. Read more about the project{' '}
   <Link to="/docs/first_glance">here</Link>, and how you can contribute to and benefit from this effort to advance the frontier in insect diversity research.
         </h4>
